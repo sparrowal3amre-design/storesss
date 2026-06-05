2761bd26adfbf2b9cb2162879b0c1e0991b98c54
sparrowal3amre-design <sparrowal3amre@gmail.com>
2026-06-05 21:31:47 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,57 @@
+const PASSWORD = "1234";
 
+function login() {
+    let p = document.getElementById("pass").value;
+
+    if (p === PASSWORD) {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("panel").style.display = "block";
+        render();
+    } else {
+        alert("خطأ");
+    }
+}
+
+function add() {
+
+    const name = document.getElementById("name").value;
+    const price = document.getElementById("price").value;
+    const file = document.getElementById("image").files[0];
+
+    const reader = new FileReader();
+
+    reader.onload = function () {
+
+        products.push({
+            id: Date.now(),
+            name,
+            price,
+            image: reader.result
+        });
+
+        save();
+        render();
+    };
+
+    reader.readAsDataURL(file);
+}
+
+function render() {
+    let box = document.getElementById("list");
+    box.innerHTML = "";
+
+    products.forEach((p, i) => {
+        box.innerHTML += `
+        <div>
+            ${p.name} - ${p.price}$
+            <button onclick="del(${i})">حذف</button>
+        </div>
+        `;
+    });
+}
+
+function del(i) {
+    products.splice(i, 1);
+    save();
+    render();
+}
