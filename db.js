1d7ac0efd36130ce5c8e6d38e3dd757cfcbae738
sparrowal3amre-design <sparrowal3amre@gmail.com>
2026-06-05 22:14:21 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,5 @@
 let users = JSON.parse(localStorage.getItem("users")) || [];
-let currentUser = JSON.parse(localStorage.getItem("currentUser")) || null;
-
 let products = JSON.parse(localStorage.getItem("products")) || [];
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 let orders = JSON.parse(localStorage.getItem("orders")) || [];
-
-function saveAll() {
-    localStorage.setItem("users", JSON.stringify(users));
-    localStorage.setItem("products", JSON.stringify(products));
-    localStorage.setItem("cart", JSON.stringify(cart));
-    localStorage.setItem("orders", JSON.stringify(orders));
-    localStorage.setItem("currentUser", JSON.stringify(currentUser));
-}
+let currentUser = JSON.parse(localStorage.getItem("currentUser")) || null;
