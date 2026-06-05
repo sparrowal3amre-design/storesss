8fb8180e59af58b1aef1d3f885fd896ae5747e4c
sparrowal3amre-design <sparrowal3amre@gmail.com>
2026-06-05 22:13:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,62 +1,76 @@
-function render() {
+function render(){
+let box = document.getElementById("products");
+box.innerHTML="";
 
-    let box = document.getElementById("products");
-    box.innerHTML = "";
-
-    products.forEach((p, i) => {
-        box.innerHTML += `
-        <div class="glass card">
-            <img src="${p.image}">
-            <h3>${p.name}</h3>
-            <p>${p.price}$</p>
-            <button onclick="add(${i})">أضف للسلة</button>
-        </div>
-        `;
-    });
+products.forEach((p,i)=>{
+box.innerHTML += `
+<div class="card">
+<img src="${p.image}">
+<h3>${p.name}</h3>
+<p>${p.price}$</p>
+<button onclick="add(${i})">Add</button>
+</div>`;
+});
 }
 
-function add(i) {
-    cart.push(products[i]);
-    saveAll();
-    updateCart();
+function add(i){
+let item = products[i];
+
+let found = cart.find(p=>p.name===item.name);
+
+if(found){
+found.qty++;
+} else {
+cart.push({...item, qty:1});
 }
 
-function updateCart() {
-
-    let box = document.getElementById("cartItems");
-    box.innerHTML = "";
-
-    let total = 0;
-
-    cart.forEach(p => {
-        total += Number(p.price);
-        box.innerHTML += `<p>${p.name} - ${p.price}$</p>`;
-    });
-
-    document.getElementById("total").innerText = "Total: " + total + "$";
+localStorage.setItem("cart",JSON.stringify(cart));
+updateCart();
 }
 
-function checkout() {
+function updateCart(){
+let box=document.getElementById("cartItems");
+let total=0;
 
-    if (!currentUser) {
-        alert("سجل دخول أولاً");
-        return;
-    }
+box.innerHTML="";
 
-    let order = {
-        user: currentUser.email,
-        items: cart,
-        total: cart.reduce((a,b)=>a+Number(b.price),0),
-        date: new Date().toLocaleString()
-    };
+cart.forEach((p,i)=>{
+total += p.price * p.qty;
 
-    orders.push(order);
+box.innerHTML += `
+<p>${p.name} ${p.price}$ × ${p.qty}
+<button onclick="removeItem(${i})">X</button>
+</p>`;
+});
 
-    cart = [];
-    saveAll();
+document.getElementById("total").innerText="Total: "+total+"$";
+}
 
-    alert("تم إرسال الطلب");
-    updateCart();
+function removeItem(i){
+cart.splice(i,1);
+localStorage.setItem("cart",JSON.stringify(cart));
+updateCart();
+}
+
+function checkout(){
+if(!currentUser){
+alert("سجل دخول");
+return;
+}
+
+orders.push({
+user:currentUser.email,
+items:cart,
+total:cart.reduce((a,b)=>a+b.price*b.qty,0),
+date:new Date().toLocaleString()
+});
+
+cart=[];
+localStorage.setItem("orders",JSON.stringify(orders));
+localStorage.setItem("cart",JSON.stringify(cart));
+
+updateCart();
+alert("تم الطلب");
 }
 
 render();
