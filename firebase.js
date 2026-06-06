e3021e3f41bad1568ea3ccc99ecddcf2c072a854
sparrowal3amre-design <sparrowal3amre@gmail.com>
2026-06-06 11:59:48 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,33 +1,42 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { 
-getFirestore, collection, addDoc, getDocs, query, where 
+
+import {
+getFirestore,
+collection,
+addDoc,
+getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 import {
 getAuth,
 createUserWithEmailAndPassword,
-signInWithEmailAndPassword,
-onAuthStateChanged,
-signOut
+signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+import {
+getStorage,
+ref,
+uploadBytes,
+getDownloadURL
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+
 const firebaseConfig = {
-apiKey: "AIzaSyBJxjMbheO6ComvGApxte6_354ML54mDXo",
-authDomain: "store-3bd3d.firebaseapp.com",
-projectId: "store-3bd3d",
-storageBucket: "store-3bd3d.firebasestorage.app",
-appId: "1:889662991855:web:3c93c89a9b2886e9005a1a"
+  /* بياناتك الحالية */
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
 
 export {
-collection, addDoc, getDocs, query, where,
+collection,
+addDoc,
+getDocs,
 createUserWithEmailAndPassword,
 signInWithEmailAndPassword,
-onAuthStateChanged,
-signOut
+ref,
+uploadBytes,
+getDownloadURL
 };
