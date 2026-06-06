9a04c6ef9f0dd2cb384c206e09f875d70bfc33c0
sparrowal3amre-design <sparrowal3amre@gmail.com>
2026-06-06 12:01:20 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -5,12 +5,41 @@ signInWithEmailAndPassword
 } from "./firebase.js";
 
 window.register = async function(){
-await createUserWithEmailAndPassword(auth,email.value,pass.value);
-alert("Account created");
+
+try{
+
+await createUserWithEmailAndPassword(
+auth,
+email.value,
+pass.value
+);
+
+alert("تم إنشاء الحساب");
+
+}catch(e){
+
+alert(e.message);
+
+}
+
 }
 
 window.login = async function(){
-await signInWithEmailAndPassword(auth,email.value,pass.value);
-alert("Login success");
+
+try{
+
+await signInWithEmailAndPassword(
+auth,
+email.value,
+pass.value
+);
+
 location.href="index.html";
+
+}catch(e){
+
+alert(e.message);
+
+}
+
 }
